16d3440b2379605e3914481d2579af3b853aca07
Lex <55722511+LexLeach@users.noreply.github.com>
2020-09-14 13:41:40 -0400

    working on character map

--- src/Character-Cards/Character-Cards.js
+++ src/Character-Cards/Character-Cards.js
@@ -1,27 +1,9 @@
 import React from 'react'
 
-export default function Characters() {
+export default function Characters({name}) {
     return (
-        <section className='section'>
-            <div className='container'>
-                <div className='columns'>
-                    <div className='column'>
-                        <div class="notification is-primary">
-                            Escanor
-                        </div>
-                    </div>
-                    <div className='column'>
-                        <div class="notification">
-                            Kyuzo
-                        </div>
-                    </div>
-                    <div className='column'>
-                        <div class="notification">
-                            Goku
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <div class="notification is-primary">
+            {name}
+        </div>
     )
   }
