d70bbb708349998b1f70951e9776ba438a1423db
Lex <55722511+LexLeach@users.noreply.github.com>
2020-09-14 14:56:43 -0400

    Updates

---
+++ precache-manifest.97803759566478e858f26db131aa87fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153c67aba87fa0191830faf7f309a12b",
+    "url": "/api-frontend-project/index.html"
+  },
+  {
+    "revision": "560361dcbf1b34b38f70",
+    "url": "/api-frontend-project/static/js/2.790b30ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api-frontend-project/static/js/2.790b30ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "199ca6e309bbad4cd30c",
+    "url": "/api-frontend-project/static/js/main.f11a9c17.chunk.js"
+  },
+  {
+    "revision": "8133e20f13d8c166ec88",
+    "url": "/api-frontend-project/static/js/runtime-main.5ba640d7.js"
+  }
+]);
