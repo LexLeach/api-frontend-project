6b94e5bebe5a38b2963afddaaf152c2b005ea33e
Lex <55722511+LexLeach@users.noreply.github.com>
2020-09-14 13:10:52 -0400

    updated character cards and hero

---
+++ src/Character-Cards/Character-Cards.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Characters() {
+    return (
+        <section className='section'>
+            <div className='container'>
+                <div className='columns'>
+                    <div className='column'>
+                        <div class="notification is-primary">
+                            Escanor
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <div class="notification">
+                            Kyuzo
+                        </div>
+                    </div>
+                    <div className='column'>
+                        <div class="notification">
+                            Goku
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+  }
