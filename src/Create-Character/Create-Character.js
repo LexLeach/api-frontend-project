9488e694e1a3990e1f167c84db7d1503cbee138b
Lex <55722511+LexLeach@users.noreply.github.com>
2020-09-15 15:54:25 -0400

    updated axios, working only for adding chars

---
+++ src/Create-Character/Create-Character.js
@@ -0,0 +1,212 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function CreateCharacter() {
+  const [modalClass, setModalClass] = useState("modal");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [bio, setBio] = useState("");
+  const [skillName, setSkillName] = useState("");
+  const [skillDescr, setSkillDescr] = useState("");
+  const [animeName, setAnimeName] = useState("");
+  const [url, setUrl] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleInput = (e, type) => {
+    e.persist();
+    const value = e.target.value;
+    handleValueUpdates[type](value);
+  };
+
+  const handleValueUpdates = {
+    name: setName,
+    age: setAge,
+    bio: setBio,
+    animeName: setAnimeName,
+    url: setUrl,
+    image: setImage,
+    skillName: setSkillName,
+    skillDescr: setSkillDescr,
+  };
+
+  const activateModal = () => {
+    setModalClass("modal is-active");
+  };
+
+  const closeModal = () => {
+    setModalClass("modal");
+  };
+
+  const createCharacter = () => {
+    axios({
+      method: "post",
+      url: "https://lexleach-api-project.herokuapp.com/characters",
+      data: {
+        name: name,
+        age: age,
+        bio: bio,
+        skills: [
+          {
+            name: skillName,
+            description: skillDescr,
+          },
+          {
+            name: "",
+            description: "",
+          },
+        ],
+        animeName: animeName,
+        url: url,
+        image: image,
+        favorited: true,
+        likes: 0,
+        dislikes: 0,
+      },
+    }).then((res) => {
+      console.log(res);
+    });
+    alert("New Character Created!");
+  };
+
+  return (
+    <div className="section has-background-white">
+      <div className="level">
+        <div className="field level-left">
+          <input
+            id="switch"
+            type="checkbox"
+            name="switch"
+            className="switch is-success is-small"
+          ></input>
+          <label htmlFor="switch">Dark Mode</label>
+        </div>
+        <button
+          className="button level-right is-primary"
+          onClick={activateModal}
+        >
+          Create Character
+        </button>
+      </div>
+      <div className={modalClass}>
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head has-background-danger">
+            <p className="modal-card-title">Create a Character</p>
+          </header>
+          <section className="modal-card-body has-text-black">
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Name"
+                  onChange={(e) => handleInput(e, "name")}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Age"
+                  onChange={(e) => handleInput(e, "age")}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Bio"
+                  onChange={(e) => handleInput(e, "bio")}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Anime Name"
+                  onChange={(e) => handleInput(e, "animeName")}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Read More URL"
+                  onChange={(e) => handleInput(e, "url")}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Image"
+                  onChange={(e) => handleInput(e, "image")}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <div className="field is-grouped">
+                  <h3>Skill</h3>
+                  <div className="level-right">
+                    <div className="buttons has-addons level-right">
+                      <button className="button is-white is-small">
+                        <span className="icon is-small has-text-success">
+                          <i className="fas fa-plus-square"></i>
+                        </span>
+                      </button>
+                      <button className="button is-white is-small">
+                        <span className="icon is-small has-text-danger-dark">
+                          <i className="fas fa-minus-square"></i>
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Skill Name"
+                  onChange={(e) => handleInput(e, "skillName")}
+                ></input>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <input
+                  className="input is-info"
+                  type="text"
+                  placeholder="Skill Description"
+                  onChange={(e) => handleInput(e, "skillDescr")}
+                ></input>
+              </div>
+            </div>
+          </section>
+          <footer className="modal-card-foot has-background-danger">
+            <div className="buttons">
+              <button className="button is-primary" onClick={closeModal}>
+                Close
+              </button>
+              <button
+                className="button is-pulled-right is-primary"
+                onClick={createCharacter}
+              >
+                Create Character
+              </button>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
