06d623d3ad70516688471b94201f74d16ce63492
Lex <55722511+LexLeach@users.noreply.github.com>
2020-09-15 23:29:51 -0400

    Updates

---
+++ precache-manifest.aed6fad7c6985bc2f6331bac3e7aa372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9feefe34367a876dc81ff431b447ffed",
+    "url": "/api-frontend-project/index.html"
+  },
+  {
+    "revision": "8a50a5d91b119abf6098",
+    "url": "/api-frontend-project/static/css/main.d2adc343.chunk.css"
+  },
+  {
+    "revision": "b8a49df60f2bf19b89c4",
+    "url": "/api-frontend-project/static/js/2.891c892b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api-frontend-project/static/js/2.891c892b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a50a5d91b119abf6098",
+    "url": "/api-frontend-project/static/js/main.c1207230.chunk.js"
+  },
+  {
+    "revision": "8133e20f13d8c166ec88",
+    "url": "/api-frontend-project/static/js/runtime-main.5ba640d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/api-frontend-project/precache-manifest.5b4402ecbcb7533ee015cb81d3d1a639.js"
+  "/api-frontend-project/precache-manifest.aed6fad7c6985bc2f6331bac3e7aa372.js"
 );
 
 self.addEventListener('message', (event) => {
